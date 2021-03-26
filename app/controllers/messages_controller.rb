456645b023b71e28c81a5f2830973110bb9f224d
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-03-26 16:45:07 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,25 @@
 class MessagesController < ApplicationController
   def index
+    # ・空のMessageモデルのインスタンス情報
+    @message = Message.new
+
+    # ・paramsに含まれているチャットルームのレコード情報
+    @room = Room.find(params[:room_id])
+  end
+  def create
+    @room = Room.find(params[:room_id])
+    # roomと紐づいて作成（よってストロングパラメータでroom記述する必要なし？←違う、form_withで取ってきてないから）
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
